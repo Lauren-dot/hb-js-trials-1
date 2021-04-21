913f5ab63e15f1ae9ac99714c5ee01a5827b7976
Lauren Ben-Shoshan <lauren@ben-shoshan.com>
2021-04-21 04:20:24 +0000

    Final Functions Completed

--- trials.py
+++ trials.py
@@ -20,7 +20,7 @@ def get_odd_indices(items):
 
     for i, item in enumerate(items):
         if i % 2 != 0:
-            result.append[item]
+            result.append(item)
 
     return result
 
@@ -37,7 +37,9 @@ def get_range(start, stop):
     nums = []
 
     for num in range(start, stop):
-        nums.append[num]
+        nums.append(num)
+
+    return nums
 
 
 def censor_vowels(word):
@@ -56,7 +58,7 @@ def snake_to_camel(string):
     camel_case = []
 
     for word in string.split("_"):
-        camel_case.append(f"{word[0].upper()}{word[1:]})
+        camel_case.append(f"{word[0].upper()}{word[1:]}")
 
     return "".join(camel_case)
 
@@ -75,13 +77,48 @@ def truncate(string):
     result = []
 
     for char in string:
-        if len(result) == 0 or char not result[-1]:
-            result.append[char]
+        if len(result) == 0 or char != result[-1]:
+            result.append(char)
+
+    return "".join(result)
 
 
 def has_balanced_parens(string):
-    pass  # TODO: replace this line with your code
+    parens = 0
+
+    for char in string:
+        if char == "(":
+            parens +=1
+        elif char == ")":
+            parens -=1
+            if parens < 0:
+                return False
+    
+    return parens == 0
 
 
 def compress(string):
-    pass  # TODO: replace this line with your code
+    compressed = []
+
+    curr_char = ""
+    char_count = 0
+
+    for char in string:
+        if char != curr_char:
+            compressed.append(curr_char)
+
+            if char_count > 1:
+                compressed.append(str(char_count))
+            
+            curr_char = char
+            char_count = 0
+
+        char_count += 1
+
+    compressed.append(curr_char)
+
+    if char_count > 1:
+        compressed.append(str(char_count))
+
+    return "".join(compressed)
+    
